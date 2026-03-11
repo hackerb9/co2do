a01d3d37e2ae3a942d32c741a1e233819a3d2cb0
b9 <hackerb9@member.fsf.org>
2026-03-10 18:45:35 -0700

    Create histogram of bytes in a .CO; calculate optimal offset
    Brian on the bitchin'100 mailing list has pointed out that, since
    certain character codes have to be escaped which adds extra bulk, we
    can shrink bang-encoded files by rotating the ASCII table by a fixed
    amount for each program. For example, the NULL character (ASCII 0) is
    extraordinarily common, but that character code is discarded by the
    M100 BASIC tokenizer. To solve this, we could add 137 to all
    characters so that NULL is represented in a BASIC loader by a
    character that does not need to be escaped.
    
    This program investigates that and sees if it is worth it. Run either
    `./histco.py [filename.co]` to check one or more files. Or, run
    `./histco.py` with no arguments to check all .CO files in the current
    working directory.
    
    Savings seem to be between 15 and 20%. So, not as good as LZ
    compression, but nothing to sneeze at, either.

---
+++ histogram/histco.py
@@ -0,0 +1,52 @@
+#!/bin/python
+from glob import glob as glob
+from sys import argv
+
+
+def accumulate_bytes(filenames):
+    for filename in filenames:
+        with open(filename, "rb") as fp:
+            buf=fp.read()
+            for x in buf:
+                sums[x]=sums[x]+1
+            
+def print_histogram_sums():
+    for x in range(256):
+        print(f'{x:3d}:\t{sums[x]:4d}')
+
+
+def getsize(sums):
+    '''Calculate the filesize after bang-encoding'''
+    total = sum([sums[x] for x in sums]) \
+        + sum([sums[x] for x in range(35) if x!=9 and x!=32])
+    return total
+
+def rotate(sums, k):
+    '''Given the table of sums for various bytes, rotate it by k'''
+    return { x:sums[(x+k)%256] for x in range(256) }
+
+if __name__ == "__main__":
+    sums={x:0 for x in range(256)}
+    if len(argv) == 1:
+        files="*.[Cc][Oo]"
+    else:
+        files=argv[1];
+
+    accumulate_bytes(glob(files))
+
+    print(f"Unrotated: {getsize(sums)} bytes.")
+    size={}
+    for k in range(256):
+        size[k]=getsize(rotate(sums, k))
+
+    m = min(size.values())
+    s = size[0]-m
+    print(f"\nCan save {s} bytes ({100*s/size[0]:.2f}%)")
+
+    for k in [ x for x in size if size[x] == m ]:
+        print(f"Rotation {k:+3d} => {size[k]} bytes.")
+        
+    m42=size[42]
+    s42=size[0]-m42
+    print(f"\nRotation of +42 would save {s42} bytes ({100*s42/size[0]:.2f}%)")
+
